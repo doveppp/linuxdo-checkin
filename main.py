365bfafe9ecc698c0738a37b195bf85dc0b0d4aa
dovepppp <1453994097@qq.com>
2024-05-02 20:26:33 +0800

    add visit topic and like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 from playwright.sync_api import sync_playwright
 
 
@@ -7,12 +8,16 @@ USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
 
 
+HOME_URL = "https://linux.do/"
+
+
 class LinuxDoBrowser:
     def __init__(self) -> None:
         self.pw = sync_playwright().start()
-        self.browser = self.pw.firefox.launch(headless=True)
-        self.page = self.browser.new_page()
-        self.page.goto("https://linux.do/")
+        self.browser = self.pw.firefox.launch(headless=False)
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page()
+        self.page.goto(HOME_URL)
 
     def login(self):
         self.page.click(".header-buttons .d-button-label")
@@ -31,10 +36,28 @@ class LinuxDoBrowser:
             print("Check in success")
             return True
 
+    def click_topic(self):
+        for topic in self.page.query_selector_all("#list-area .title"):
+            page = self.context.new_page()
+            page.goto(HOME_URL + topic.get_attribute("href"))
+            if random.random() < 0.02:  # 100 * 0.02 * 30 = 60
+                self.click_like(page)
+            time.sleep(10)
+            page.close()
+
+    def run(self):
+        if not self.login():
+            return
+        self.click_topic()
+
+    def click_like(self, page):
+        page.locator(".discourse-reactions-reaction-button").first.click()
+        print("Like success")
+
 
 if __name__ == "__main__":
     if not USERNAME or not PASSWORD:
         print("Please set USERNAME and PASSWORD")
         exit(1)
     l = LinuxDoBrowser()
-    l.login()
+    l.run()
