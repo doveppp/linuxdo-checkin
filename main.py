97a0ffa4806136240624c7f66d3c4e3572aff193
dovepppp <1453994097@qq.com>
2024-05-02 16:50:06 +0800

    first

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import time
+from playwright.sync_api import sync_playwright
+
+
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+
+
+class LinuxDoBrowser:
+    def __init__(self) -> None:
+        self.pw = sync_playwright().start()
+        self.browser = self.pw.firefox.launch(headless=False)
+        self.page = self.browser.new_page()
+        self.page.goto("https://linux.do/")
+
+    def login(self):
+        self.page.click(".header-buttons .d-button-label")
+        time.sleep(2)
+        self.page.fill("#login-account-name", USERNAME)
+        time.sleep(2)
+        self.page.fill("#login-account-password", PASSWORD)
+        time.sleep(2)
+        self.page.click("#login-button")
+        time.sleep(30)
+        user_ele = self.page.query_selector("#current-user")
+        if not user_ele:
+            print("Login failed")
+            return False
+        else:
+            print("Check in success")
+            return True
+
+        input()
+
+
+if __name__ == "__main__":
+    if not USERNAME or not PASSWORD:
+        print("Please set USERNAME and PASSWORD")
+        exit(1)
+    l = LinuxDoBrowser()
+    l.login()
